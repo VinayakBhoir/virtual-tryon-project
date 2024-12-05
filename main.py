1d8aaa881fa06933a6920da61e13c394b0b6d124
VinayakBhoir <142589273+VinayakBhoir@users.noreply.github.com>
2024-12-05 17:26:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -122,5 +122,6 @@ def change_shirt():
     return jsonify({'status': 'success'})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
